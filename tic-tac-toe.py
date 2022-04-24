09f3ab72e83f1a49b9a7367a46ddf611882d0a84
zaserge <zaserge@gmail.com>
2022-04-24 12:28:27 +0300

    Add Greeting message. Reformat.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -21,11 +21,12 @@
 
 
 import random
+from datetime import datetime
 
-VERSION = "0.1"
+VERSION = "0.2"
 
-USER = {'name': "Human", 'mark': 'x'}
-ROBOT = {'name': "Robot", 'mark': 'o'}
+USER = {"name": "Human", "mark": "x"}
+ROBOT = {"name": "Robot", "mark": "o"}
 
 LINE = 0
 N_MARKS = 1
@@ -33,71 +34,85 @@ N_MARKS = 1
 CELL_NUMBER = 0
 CELL_VALUE = 1
 
-EMPTY = ' '
+# EMPTY = "·"
+EMPTY = "."
 
-table = {7: EMPTY, 8: EMPTY, 9: EMPTY, 
-         4: EMPTY, 5: EMPTY, 6: EMPTY, 
-         1: EMPTY, 2: EMPTY, 3: EMPTY}
+table = {
+    7: 7, 8: 8, 9: 9,
+    4: 4, 5: 5, 6: 6,
+    1: 1, 2: 2, 3: 3,
+}
 
 winning_lines = [
-    {7, 8, 9}, # up row 
-    {4, 5, 6}, # center row
-    {1, 2, 3}, # bottom row
-    {7, 4, 1}, # left column
-    {8, 5, 2}, # center column
-    {9, 6, 3}, # right column
-    {7, 5, 3}, # first diagonal
-    {9, 5, 1}  # second diagonal
+    {7, 8, 9},  # up row
+    {4, 5, 6},  # center row
+    {1, 2, 3},  # bottom row
+    {7, 4, 1},  # left column
+    {8, 5, 2},  # center column
+    {9, 6, 3},  # right column
+    {7, 5, 3},  # first diagonal
+    {9, 5, 1},  # second diagonal
 ]
 
+
 def draw_table():
-    """! Draw game's table
+    """Draw game's table
 
     @return None
     """
-    print("  1 2 3")
-    print(f"1 {table[7]} {table[8]} {table[9]}")
-    print(f"2 {table[4]} {table[5]} {table[6]}")
-    print(f"3 {table[1]} {table[2]} {table[3]}")
-    
+    print("┌───────┐")
+    print(f"│ {table[7]} {table[8]} {table[9]} │")
+    print(f"│ {table[4]} {table[5]} {table[6]} │")
+    print(f"│ {table[1]} {table[2]} {table[3]} │")
+    print("└───────┘")
+
 
 def check_all_lines(mark: str) -> list:
-    """! check all lines 
+    """Check all lines
+
+    @param mark {str} Mark to search at table
 
     @return List of tuples (number line, matched marks number)
     """
     return [
         (n, sum([table[cell] == mark for cell in line]))
         for n, line in enumerate(winning_lines)
-        ]
+    ]
 
 
 def is_winner(mark: str) -> int:
-    """! Check if any line contains 3 matches
-    
+    """Check if any line contains 3 matches
+
+    @param mark {str} Mark to search at table
+
     @return Line number or None
     """
-    sorted_lines = sorted(check_all_lines(mark), key=lambda item : item[N_MARKS], reverse=True)
+    sorted_lines = sorted(
+        check_all_lines(mark), key=lambda item: item[N_MARKS], reverse=True
+    )
     return sorted_lines[0][LINE] if sorted_lines[0][N_MARKS] == 3 else None
 
 
-def proceed_turn(choice: int, gamer: set) -> bool:
-    """! Do game
+def proceed_turn(choice: int, gamer: dict) -> bool:
+    """Do game
+
+    @param choice {int} Gamer's choice
+    @param gamer {dict} Gamer's name and mark
 
     @return True if game ends otherwise False
     """
     if choice == 0:
         draw_table()
-        print (f"{gamer['name']} gave up")
+        print(f"{gamer['name']} gave up")
         return True
     else:
-        table[choice] = gamer['mark']
+        table[choice] = gamer["mark"]
 
-        if (l := is_winner(gamer['mark'])) is not None:
+        if (l := is_winner(gamer["mark"])) is not None:
             for cell in winning_lines[l]:
-                table[cell] = gamer['mark'].upper()
+                table[cell] = gamer["mark"].upper()
             draw_table()
-            print (f"{gamer['name']} is winner!!!")
+            print(f"{gamer['name']} is winner!!!")
             return True
 
     # if no free cell then a draw
@@ -110,8 +125,9 @@ def proceed_turn(choice: int, gamer: set) -> bool:
         print("A draw")
         return True
 
+
 def user_turn() -> int:
-    """! Read user choice and do some basic checks
+    """Read user choice and do some basic checks
 
     @return Choice
     """
@@ -130,20 +146,25 @@ def user_turn() -> int:
             continue
         elif table[choice] != EMPTY:
             print("Cell is occupied, try another.")
-            continue        
+            continue
         else:
             return choice
 
 
 DO_ROBOT_GAVE_UP = False
 
+
 def robot_turn() -> int:
     """! Make robot choice. very basic ai
 
     @return Choice
     """
-    sorted_users_lines = sorted(check_all_lines(USER['mark']), key=lambda item : item[1], reverse=True)
-    sorted_robots_lines = sorted(check_all_lines(ROBOT['mark']), key=lambda item : item[1], reverse=True)
+    sorted_users_lines = sorted(
+        check_all_lines(USER["mark"]), key=lambda item: item[1], reverse=True
+    )
+    sorted_robots_lines = sorted(
+        check_all_lines(ROBOT["mark"]), key=lambda item: item[1], reverse=True
+    )
 
     # looking for robot's 2 marks line and win
 
@@ -172,22 +193,38 @@ def robot_turn() -> int:
 
     # make choice to random free cell
 
-    # TODO: не ставить если есть в линии враги 
+    # TODO: не ставить если есть в линии враги
 
-    return random.choice([cell[CELL_NUMBER] for cell in table.items() if cell[CELL_VALUE] == EMPTY])
+    # TODO: играть по стратегии "победа или ничья"
 
-
-
+    return random.choice(
+        [cell[CELL_NUMBER]
+            for cell in table.items() if cell[CELL_VALUE] == EMPTY]
+    )
 
 
 #
-# Start game 
+# Start game
 #
+
+
+print("Hello. Let's play Tic-Tac-Toe")
+print("Cells numbered like digits at keyboard")
+draw_table()
+name = input("Enter your name (or skip to use default):")
+if name:
+    USER["name"] = name
+
+# init random numbers generator
+random.seed(datetime.now().timestamp())
+for cell in table.keys():
+    table[cell] = EMPTY
+
 turn = 1
 while True:
     draw_table()
 
-    print(f"Turn #{turn}: {USER['name']}") 
+    print(f"Turn #{turn}: {USER['name']}")
     choice = user_turn()
     if proceed_turn(choice, USER):
         break
